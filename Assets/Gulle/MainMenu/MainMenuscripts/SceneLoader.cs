74dd37a24c2a1000e01cd1e8a910b29053f46b41
Gustav <gustavheinerhenriksen@gmail.com>
2025-03-19 16:12:15 +0100

    sussy animations

--- Assets/Gulle/MainMenu/MainMenuscripts/SceneLoader.cs
+++ Assets/Gulle/MainMenu/MainMenuscripts/SceneLoader.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneLoader : MonoBehaviour
 {
+    public Animator animator;
+    
     public void LoadScene(string sceneName)
     {
+        StartCoroutine(FadeAndLoadScene(sceneName));
+    }
+
+    private IEnumerator FadeAndLoadScene(string sceneName)
+    {
+        animator.SetTrigger("FadeOut");
+
+        // Wait until the fade-out animation is done
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
         SceneManager.LoadScene(sceneName);
     }
 }
