6bdfdb02217b7d4f42c359c6119de0b6ac4b10dc
FrederikMoestrup <cph-fm124@cphbusiness.dk>
2025-03-26 15:28:43 +0100

    StatusEffectable done

--- Assets/Scripts/CharacterComponents/StatusEffectable.cs
+++ Assets/Scripts/CharacterComponents/StatusEffectable.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
 using Scripts.CharacterComponents;
+using UnityEngine.AI;
 
 
 public class StatusEffectable : MonoBehaviour
 {
     private StatusEffectScriptableObject currentEffect;
     
+    [SerializeField] private Transform effectPoint;
+    
     private float currentEffectTime = 0f;
     private float nextTickTime = 0f;
+    private float originalSpeed;
+    private bool slowed = false;
+    private GameObject effectInstance;
     
     void Update()
     {
@@ -15,21 +21,34 @@ public class StatusEffectable : MonoBehaviour
         {
             HandleEffect();
         }
+        
+        if (effectInstance != null)
+        {
+            effectInstance.transform.position = effectPoint.position;
+        }
     }
     
     public void ApplyEffect(StatusEffectScriptableObject effect)
     {
+        if(slowed) RemoveSlow();
+        if(effectInstance!=null) Destroy(effectInstance);
         this.currentEffect=effect;
+        this.currentEffectTime = 0f;
+        this.nextTickTime = currentEffect.TickSpeed;
+
+        SpawnEffect(currentEffect.EffectParticles, currentEffect.Lifetime);
+        ApplySlow();
     }
 
-    public void RemoveEffect()
+    private void RemoveEffect()
     {
+        if(slowed) RemoveSlow();
         this.currentEffect=null;
         currentEffectTime = 0;
         nextTickTime = 0;
     }
     
-    public void HandleEffect()
+    private void HandleEffect()
     {
         if (currentEffect != null)
         {
@@ -40,14 +59,64 @@ public class StatusEffectable : MonoBehaviour
             }
             if (currentEffect == null) return;
             
-            if(currentEffect.DOTAmount!=0 && currentEffectTime > nextTickTime)
+            HandleDot();
+            HandleHot();
+            
+        }
+    }
+    
+    private void HandleDot()
+    {
+        if(currentEffect.DOTAmount!=0 && currentEffectTime > nextTickTime)
+        {
+            nextTickTime += currentEffect.TickSpeed;
+            gameObject.GetComponent<Health>().TakeDamage(currentEffect.DOTAmount);
+        }
+    }
+    
+    private void HandleHot()
+    {
+        if(currentEffect.HOTAmount!=0 && currentEffectTime > nextTickTime)
+        {
+            nextTickTime += currentEffect.TickSpeed;
+            gameObject.GetComponent<Health>().Heal(currentEffect.HOTAmount);
+        }
+    }
+    
+    private void ApplySlow()
+    {
+        if (currentEffect.MovementPenalty != -1)
+        {
+            NavMeshAgent agent = gameObject.GetComponent<NavMeshAgent>();
+            if (agent != null)
             {
-                nextTickTime += currentEffect.TickSpeed;
-                gameObject.GetComponent<Health>().TakeDamage(currentEffect.DOTAmount);
+                originalSpeed = agent.speed;
+                agent.speed *= currentEffect.MovementPenalty;
+                slowed = true;
             }
         }
     }
     
+    private void RemoveSlow()
+    {
+        if (currentEffect.MovementPenalty != -1)
+        {
+            NavMeshAgent agent = gameObject.GetComponent<NavMeshAgent>();
+            if (agent != null)
+            {
+                agent.speed = originalSpeed;
+                slowed = false;
+            }
+        }
+    }
     
+    private void SpawnEffect(GameObject effectPrefab, float lifetime)
+    {
+        if (effectPrefab != null)
+        {
+            effectInstance = Instantiate(effectPrefab, effectPoint.position, effectPoint.rotation);
+            Destroy(effectInstance, lifetime);
+        }
+    }
     
 }
--- Assets/Scripts/Spells/StatusEffectSystem/StatusEffectScriptableObject.cs
+++ Assets/Scripts/Spells/StatusEffectSystem/StatusEffectScriptableObject.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Status Effect", menuName = "Status Effects")]
 public class StatusEffectScriptableObject : ScriptableObject
 {
-    public int DOTAmount;
-    public float TickSpeed;
-    public float MovementPenalty;
-    public float Lifetime;
+    public int DOTAmount = 0;
+    public int HOTAmount = 0;
+    public float TickSpeed = 1;
+    public float MovementPenalty = -1f;
+    public float Lifetime = 1;
     
     public GameObject EffectParticles;
 }
